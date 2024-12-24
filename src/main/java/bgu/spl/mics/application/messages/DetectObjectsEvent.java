0157d2602f230d5977f7cdbca6bd520926ffb1b0
GUY <guy1998@gmail.com>
2024-12-24 20:19:07 +0200

    finish day 2

--- src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
@@ -1,16 +1,17 @@
 package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 import bgu.spl.mics.application.objects.StampedCloudPoints;
+import bgu.spl.mics.application.objects.StampedDetectedObject;
 
 import java.util.List;
 
 public class DetectObjectsEvent implements Event<StampedCloudPoints> {
-    private final StampedDetectedObjects stampedDetectedObjects;
+    private final StampedDetectedObject stampedDetectedObjects;
 
-    public DetectObjectsEvent(StampedDetectedObjects stampedDetectedObjects) {
+    public DetectObjectsEvent(StampedDetectedObject stampedDetectedObjects) {
         this.stampedDetectedObjects = stampedDetectedObjects;
     }
 
-    public StampedDetectedObjects getStampedDetectedObjects() {
+    public StampedDetectedObject getStampedDetectedObjects() {
         return stampedDetectedObjects;
 }//יש לנו את זה בעבודה רק צריך לממש את המחלקה אל תבהלו 
